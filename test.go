7770a31663c60080a130b78677ccf5fb88279dd9
Zhao JiaJin <zhaojj@rd.netease.com>
2018-03-09 13:59:08 +0800

    reorder list

--- test.go
+++ test.go
@@ -2495,6 +2495,44 @@ func wbhlp(s string, wordDict []string,m map[string][]string)[]string{
 	return ret
 }
 
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func reorderList(head *ListNode)  {
+	if head == nil || head.Next == nil{
+		return 
+	}
+	p1,p2 := head,head
+	for p2.Next != nil && p2.Next.Next != nil{
+		p1 = p1.Next
+		p2 = p2.Next.Next
+	}
+
+	preMiddle := p1
+	preCurrent := p1.Next
+	for preCurrent.Next != nil{
+		current := preCurrent.Next
+		preCurrent.Next = current.Next
+		current.Next = preMiddle.Next
+		preMiddle.Next = current
+	}
+
+	p1 = head
+	p2 = preMiddle.Next
+	for p1 != preMiddle{
+		preMiddle.Next = p2.Next
+		p2.Next = p1.Next
+		p1.Next = p2
+		p1 = p2.Next
+		p2 = preMiddle.Next
+	}
+
+}
+
 
 func main() {
 	fmt.Println(wordBreak("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",[]string{"a","aa","aaa","aaaa","aaaaa","aaaaaa","aaaaaaa","aaaaaaaa","aaaaaaaaa","aaaaaaaaaa"}))
